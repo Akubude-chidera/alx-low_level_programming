818fa5b110fcff701b9873a1921c8c0fb04c9b58
Chidera Akubude <akubudechyboi00@gmail.com>
2022-07-01 12:38:23 -0700

    hmm

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main block
+ * Description: write the alphabets in reverse order
+ * Return: 0
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z'; ch >= 'a'; ch--)
+	{
+	       putchar(ch);
+	}
+
+	putchar('\n');
+	return (0);
+}
+
+
