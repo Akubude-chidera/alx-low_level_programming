fc96ca8247d0249f59dd1ea3cf7698de6259c2c5
Chidera Akubude <akubudechyboi00@gmail.com>
2022-07-01 08:54:10 -0700

    digits

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -24,7 +24,7 @@ int main(void)
 	}
 	else if ((n < 6) && (n != 0))
 	{
-		printf("Last digit of %d and is less than 6 and not zero\n", n)
+		printf("Last digit of %d and is less than 6 and not zero\n", n);
 	}
 	return (0);
 }
