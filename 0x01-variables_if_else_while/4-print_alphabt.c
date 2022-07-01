ace9f4a246b0197e14955920f4dd82a2daf99028
Chidera Akubude <akubudechyboi00@gmail.com>
2022-07-01 11:11:25 -0700

    without q and e

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,11 +8,14 @@
 
 int main(void)
 {
-	char ch;
+	char ch, e, q;
+
+	e = 'e';
+	q = 'q';
 
 	for (ch = 'a'; ch <= 'z'; ch++)
 	{
-		if (ch != 'e' || ch != 'q')
+		if (ch != e && ch != q)
 		putchar(ch);
 
 	}
