1ef5284a63c47545f8af08090b0f7280b119f667
Chidera Akubude <akubudechyboi00@gmail.com>
2022-07-01 11:19:24 -0700

    0-9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main block
+ * Description: print all single digit in base 10
+ * Return: 0
+ */
+
+int main(void)
+{
+	int d;
+
+	for (int d = 0; d < 10; d++)
+		printf("%d\n", d);
+	return (0);
+}
