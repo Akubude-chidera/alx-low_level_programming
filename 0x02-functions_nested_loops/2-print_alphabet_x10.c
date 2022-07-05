a7144b369aa65bb508e3b8084a3b5f3df38681f2
Chidera Akubude <akubudechyboi00@gmail.com>
2022-07-05 17:09:14 +0100

    10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet - in lower case
+ */
+void print_alphabet_x10(void);
+{
+	int c;
+	int d;
+
+	for (d = 0; d < 10; d++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		-putchar('\n');
+	}
+}
