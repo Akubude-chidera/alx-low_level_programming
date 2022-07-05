6311a6c56ca845ae6f6e027825fc9d28b40807ec
Chidera Akubude <akubudechyboi00@gmail.com>
2022-07-05 19:23:21 +0100

    to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_to_98 - prints to 98
+ * @n: the starting point
+ */
+
+void print_to_98(int n)
+{
+	while (n <= 98)
+	{
+		printf("%d ,", n);
+		n++;
+	}
+	while (n >= 98)
+	{
+		printf("%d, ", n);
+		n--;
+	}
+
+	putchar('\n');
+}
