9e68f24bc2ad2850658490857dd557ad75de6f2b
Chidera Akubude <akubudechyboi00@gmail.com>
2022-07-07 17:43:04 +0100

    test3

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - prints the numbers from 1 to 100, followed by a new line
- * but for multiples of three prints Fizz instead of the number
- * and for the multiples of five prints Buzz
- * Return: void
+ * main - check the code for alx students.
+ *
+ * Return: void.
  */
 
 int main(void)
