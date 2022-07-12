354df1d96d1810d194bc149a7918b4951126e3b3
Chidera Akubude <akubudechyboi00@gmail.com>
2022-07-12 17:25:26 +0100

    rev

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * rev_string - reversing a string
+ * @s: parameter s
+ */
+
+void rev_string(char *s)
+{
+	char temp;
+	int i, length1, length2, half;
+
+	length1 = 0;
+	length2 = 0;
+	half = length1 / 2;
+
+	while (s[length1] != '\0')
+		length1++;
+
+	length2 = length1 - 1;
+
+	for (i = 0; i < half; i++)
+	{
+		temp = s[i];
+		s[i] = s[length2];
+		s[length2] = temp;
+		length2 -= 1;
+	}
+}
