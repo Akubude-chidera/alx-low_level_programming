fd54f884d4438efa706d0eff2f26a6611894b922
Chidera Akubude <akubudechyboi00@gmail.com>
2022-07-12 18:08:58 +0100

    second half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * puts_half - prints second half of the string
+ * @str: str parameter
+ */
+
+void puts_half(char *str)
+{
+	int i, j, k;
+
+	i = strlen(str);
+
+	if (i % 2 == 1)
+		j = i / 2 + 1;
+	else
+		j = i / 2;
+
+	for (k = j; k < i; k++)
+		_putchar(str[i]);
+
+	_putchar('\n');
+}
