8e422b0190a1fd33b78db389b11e5b3b6a08cd35
Chidera Akubude <akubudechyboi00@gmail.com>
2022-07-18 14:20:14 +0100

    task 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _memset - this memory set function
+ * @s: string param
+ * @b: character param
+ * @n: an integer
+ * Return: a string
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	return (memset(s,b,n));
+}
